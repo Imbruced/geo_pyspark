3d36c887b81f430c63fb63c7723a61a91ad415f8
Pawel <pawel93kocinski@gmail.com>
2019-08-26 23:34:32 +0200

    tests

---
+++ tests/test_convertion.py
@@ -0,0 +1,28 @@
+import os
+from unittest import TestCase
+
+from pyspark.sql import SparkSession
+
+from geo_pyspark.registrator import GeoSparkRegistrator
+
+spark = SparkSession.builder.\
+        config("--master", "local").\
+        getOrCreate()
+
+
+class TestGeometryConvert(TestCase):
+
+    def test_register_functions(self):
+        os.environ["JAVA_HOME"] = "C:\\Program Files\\AdoptOpenJDK\\jdk-8.0.212.04-hotspot"
+        GeoSparkRegistrator.registerAll(spark)
+        df = spark.sql("""SELECT st_geomfromtext('POINT(-6.0 52.0)') as geom""")
+        df.show()
+
+    def test_geometry_factory(self):
+        pass
+
+    def test_collect(self):
+        os.environ["JAVA_HOME"] = "C:\\Program Files\\AdoptOpenJDK\\jdk-8.0.212.04-hotspot"
+        GeoSparkRegistrator.registerAll(spark)
+        df = spark.sql("""SELECT st_geomfromtext('POINT(-6.0 52.0)') as geom""")
+        df.collect()
