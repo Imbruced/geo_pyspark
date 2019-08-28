04d2b8a6a3d347848ea18e33b958b046a454126f
Pawel <pawel93kocinski@gmail.com>
2019-08-28 21:55:38 +0200

    Allowing not only byter array but also list of bytes

--- utils/binary_parser.py
+++ utils/binary_parser.py
@@ -1,21 +1,31 @@
 import struct
+from typing import List, Union
 
 import attr
+
+DOUBLE_SIZE = 8
+INT_SIZE = 4
 
 
 @attr.s
 class BinaryParser:
 
     @classmethod
-    def read_double(cls, bytes: bytearray):
+    def read_double(cls, bytes: Union[bytearray, List[int]]):
+        if type(bytes) == list:
+            bytes = bytearray(bytes)
         return struct.unpack("d", bytes)
 
     @classmethod
-    def read_int(cls, bytes: bytearray):
+    def read_int(cls, bytes: Union[bytearray, List[int]]):
+        if type(bytes) == list:
+            bytes = bytearray(bytes)
         return struct.unpack("i", bytes)
 
     @classmethod
-    def read_byte(cls, bytes: bytearray):
+    def read_byte(cls, bytes: Union[bytearray, List[int]]):
+        if type(bytes) == list:
+            bytes = bytearray(bytes)
         return struct.unpack("b", bytes)
 
     @classmethod
