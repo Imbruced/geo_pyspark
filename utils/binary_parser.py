627b4ec65f369dbbbc8232598bd41905af64ff01
Pawel <pawel93kocinski@gmail.com>
2019-08-28 21:51:55 +0200

    Fixed issue with removing negative bytearray value.

--- utils/binary_parser.py
+++ utils/binary_parser.py
@@ -17,3 +17,12 @@ class BinaryParser:
     @classmethod
     def read_byte(cls, bytes: bytearray):
         return struct.unpack("b", bytes)
+
+    @classmethod
+    def remove_negatives(cls, bytes):
+        return [cls.remove_negative(bt) for bt in bytes]
+
+    @classmethod
+    def remove_negative(cls, byte):
+        bt_pos = byte if byte >= 0 else byte + 256
+        return bt_pos
