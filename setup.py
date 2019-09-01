3cd9c36bff6600da4f3361ea3f2c90e7cc000d92
pawel93kocinski@gmail.com <pawel93kocinski@gmail.com>
2019-09-01 17:05:17 +0200

    Setup file for lib, initial push

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+
+setup(
+    name='geo_pyspark',
+    version='0.1.0',
+    description='GeoSpark Python Wrapper',
+    url='https://github.com/Imbruced/geo_pyspark',
+    author='Pawel Kocinski',
+    author_email='pawel93kocinski@gmail.com',
+    packages=find_packages(exclude=['register', 'docs', 'tests']),
+    python_requires='>=3.6',
+    install_requires=['pyspark', 'findspark', 'pandas', 'geopandas'],
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/Imbruced/geo_pyspark'
+    },
+)
