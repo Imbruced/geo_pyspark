736412473f77c75efccbba617a444e3037962a08
Pawel <pawel93kocinski@gmail.com>
2019-09-02 22:58:43 +0200

    Added function create_data_files which responsibility is to copy jars files to installation place.

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
+import os
+from typing import Tuple, List
+
 from setuptools import setup, find_packages
 from os import path
 
 here = path.abspath(path.dirname(__file__))
+jars_relative_path = "geo_pyspark/jars"
+
+
+def create_data_files() -> List[Tuple[str, List[str]]]:
+    data_files = []
+    jars_path = os.path.join(here, jars_relative_path)
+    for version in os.listdir(jars_path):
+        version_path = os.path.join(jars_path, version)
+        jar_files = [os.path.join(jars_relative_path, version, jar_file) for jar_file in os.listdir(version_path)]
+        print(jar_files)
+        data_files.append((os.path.join(jars_relative_path, version), jar_files))
+    return data_files
+
 
 setup(
     name='geo_pyspark',
@@ -16,4 +32,5 @@ setup(
     project_urls={
         'Bug Reports': 'https://github.com/Imbruced/geo_pyspark'
     },
+    data_files=create_data_files()
 )
