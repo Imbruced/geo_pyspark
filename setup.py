2a58355c311e7157080aca08cade40f3eff50419
pawel93kocinski@gmail.com <pawel93kocinski@gmail.com>
2019-09-02 22:17:56 +0200

    Setpu file update

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-
 setup(
     name='geo_pyspark',
     version='0.1.0',
@@ -11,7 +10,7 @@ setup(
     url='https://github.com/Imbruced/geo_pyspark',
     author='Pawel Kocinski',
     author_email='pawel93kocinski@gmail.com',
-    packages=['geo_pyspark'],
+    packages=find_packages(exclude=['geo_pyspark.data', 'geo_pyspark.tests']),
     python_requires='>=3.6',
     install_requires=['pyspark', 'findspark', 'pandas', 'geopandas'],
     project_urls={
