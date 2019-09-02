829b4521f30f2e889e7e7c9fae25f7e0e88325c7
pawel93kocinski@gmail.com <pawel93kocinski@gmail.com>
2019-09-02 22:18:23 +0200

    added full path to jars

--- geo_pyspark/register/__init__.py
+++ geo_pyspark/register/__init__.py
@@ -1,4 +1,5 @@
 import os
+from os import path
 
 import findspark
 
@@ -11,9 +12,12 @@ def find_spark_version() -> str:
     return ".".join(major_version)
 
 
+abs_path = path.abspath(path.dirname(__file__))
+module_path = os.path.join(*os.path.split(abs_path)[:-1])
 spark_version = find_spark_version()
+jars_path = os.path.join(module_path, "jars", spark_version)
 
-findspark.add_jars(f"geo_pyspark/jars/{spark_version}/*")
+findspark.add_jars(os.path.join(jars_path, "*"))
 findspark.init(os.environ["SPARK_HOME"])
 
 __all__ = ["GeoSparkRegistrator"]
