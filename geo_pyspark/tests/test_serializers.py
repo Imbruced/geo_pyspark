f4316c8a96daeb06cf0dcbf573ef523169e4ec35
Pawel <pawel93kocinski@gmail.com>
2019-09-09 22:08:19 +0200

    added tests for point serialization

--- geo_pyspark/tests/test_serializers.py
+++ geo_pyspark/tests/test_serializers.py
@@ -1,8 +1,15 @@
 from unittest import TestCase
 from pyspark.sql import SparkSession
+from pyspark.sql import types as t
+from shapely.geometry import Point
 
 from geo_pyspark.utils.serde import KryoSerializer, GeoSparkKryoRegistrator
 from geo_pyspark.register import GeoSparkRegistrator
+from geo_pyspark.sql.types import GeometryType
+
+
+spark = SparkSession.builder.\
+        getOrCreate()
 
 
 class TestGeoSparkKryo(TestCase):
@@ -13,3 +20,17 @@ class TestGeoSparkKryo(TestCase):
             config("spark.serializer", KryoSerializer.getName).\
             config("spark.kryo.registrator", GeoSparkKryoRegistrator.getName).\
             getOrCreate()
+
+    def test_serialize_point(self):
+        data = [[1, "name", Point(21.0, 52.0)]]
+        schema = t.StructType(
+            [
+                t.StructField("id", t.IntegerType(), False),
+                t.StructField("name", t.StringType(), False),
+                t.StructField("location", GeometryType(), False)
+            ]
+        )
+        spark.createDataFrame(
+            data,
+            schema
+        ).show()
