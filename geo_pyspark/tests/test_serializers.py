8b9ae80d3c11efcd2afd816d6a834b1a9f88ca9e
pawel93kocinski@gmail.com <pawel93kocinski@gmail.com>
2019-09-28 23:44:15 +0200

    Added test for Point serialization.

--- geo_pyspark/tests/test_serializers.py
+++ geo_pyspark/tests/test_serializers.py
@@ -1,35 +1,30 @@
 from unittest import TestCase
-from pyspark.sql import SparkSession
-from pyspark.sql import types as t
-from shapely.geometry import Point
 
-from geo_pyspark.utils.serde import KryoSerializer, GeoSparkKryoRegistrator
+from pyspark.sql.types import IntegerType
+
 from geo_pyspark.register import GeoSparkRegistrator
 from geo_pyspark.sql.types import GeometryType
-
+from shapely.geometry import Point
+from pyspark.sql import types as t, SparkSession
 
 spark = SparkSession.builder.\
         getOrCreate()
 
+GeoSparkRegistrator.registerAll(spark)
 
-class TestGeoSparkKryo(TestCase):
 
-    def test_kryo_registrator(self):
-        spark = SparkSession.builder.\
-            master("local[2]").\
-            config("spark.serializer", KryoSerializer.getName).\
-            config("spark.kryo.registrator", GeoSparkKryoRegistrator.getName).\
-            getOrCreate()
+class TestsSerializers(TestCase):
 
-    def test_serialize_point(self):
-        data = [[1, "name", Point(21.0, 52.0)]]
+    def test_point_serializer(self):
+        data = [[1, Point(21.0, 56.0)]]
+
         schema = t.StructType(
             [
-                t.StructField("id", t.IntegerType(), False),
-                t.StructField("name", t.StringType(), False),
-                t.StructField("location", GeometryType(), False)
+                t.StructField("id", IntegerType(), True),
+                t.StructField("geom", GeometryType(), True)
             ]
         )
+
         spark.createDataFrame(
             data,
             schema
