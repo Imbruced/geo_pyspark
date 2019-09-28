dfe256d170d055034efa4818a839c7272ae8d8fd
pawel93kocinski@gmail.com <pawel93kocinski@gmail.com>
2019-09-28 22:11:09 +0200

    Created tests for geometry objects, serialization to GeoSpark frames and converting to geopandas dataframe.

---
+++ geo_pyspark/tests/test_shapely_inherits.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from shapely.geometry import Point as ShapelyPoint
+
+from geo_pyspark.sql.base_geom import Point
+from geo_pyspark.sql.types import GeometryType
+
+
+class TestGeometries(TestCase):
+
+    def test_point(self):
+        point = Point(21.0, 52.0)
+        self.assertEqual(isinstance(point, ShapelyPoint), True)
+        self.assertEqual(type(point.__UDT__), GeometryType)
+
+    def test_multipoint(self):
+        pass
+
+    def test_linestring(self):
+        pass
+
+    def test_multilinestring(self):
+        pass
+
+    def test_polygon(self):
+        pass
+
+    def test_multipolygon(self):
+        pass
+
+    def test_convert_to_geopandas(self):
+        pass
+
+    def test_convert_to_geospark(self):
+        pass
