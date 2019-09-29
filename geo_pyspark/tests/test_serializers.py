1bdffe4a97c6f1936b6392dc96f0db7d1eb970f5
pawel93kocinski@gmail.com <pawel93kocinski@gmail.com>
2019-09-29 10:32:57 +0200

    Test for point serializer.

--- geo_pyspark/tests/test_serializers.py
+++ geo_pyspark/tests/test_serializers.py
@@ -16,16 +16,25 @@ GeoSparkRegistrator.registerAll(spark)
 class TestsSerializers(TestCase):
 
     def test_point_serializer(self):
-        data = [[1, Point(21.0, 56.0)]]
+        data = [
+            [1, Point(21.0, 56.0), Point(21.0, 59.0)]
+
+        ]
 
         schema = t.StructType(
             [
                 t.StructField("id", IntegerType(), True),
-                t.StructField("geom", GeometryType(), True)
+                t.StructField("geom_from", GeometryType(), True),
+                t.StructField("geom_to", GeometryType(), True)
             ]
         )
-
         spark.createDataFrame(
             data,
             schema
-        ).show()
+        ).createOrReplaceTempView("points")
+
+        distance = spark.sql(
+            "select st_distance(geom_from, geom_to) from points"
+        ).collect()[0][0]
+        print(distance)
+        self.assertEqual(distance, 3.0)
