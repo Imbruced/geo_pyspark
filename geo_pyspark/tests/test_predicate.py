c4e49de4434a3b6e25007502661c6df6f63df65d
pawel93kocinski@gmail.com <pawel93kocinski@gmail.com>
2019-10-04 20:10:13 +0200

    Added test st_equals_for_st_point.

--- geo_pyspark/tests/test_predicate.py
+++ geo_pyspark/tests/test_predicate.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from pyspark.sql import SparkSession
 
-from geo_pyspark.data import csv_point_input_location
+from geo_pyspark.data import csv_point_input_location, csv_point1_input_location
 from geo_pyspark.register import GeoSparkRegistrator
 
 spark = SparkSession.builder. \
@@ -66,3 +66,22 @@ class TestPredicate(TestCase):
             "select * from pointdf where ST_Within(pointdf.arealandmark, ST_PolygonFromEnvelope(1.0,100.0,1000.0,1100.0))")
         result_df.show()
         assert (result_df.count() == 999)
+
+    def test_st_equals_for_st_point(self):
+        point_df_csv = spark.read.\
+            format("csv").\
+            option("delimiter", ",").\
+            option("header", "false").load(
+                csv_point1_input_location
+            )
+
+        point_df_csv.createOrReplaceTempView("pointtable")
+
+        point_df = spark.sql(
+            "select ST_Point(cast(pointtable._c0 as Decimal(24,20)),cast(pointtable._c1 as Decimal(24,20))) as point from pointtable")
+        point_df.createOrReplaceTempView("pointdf")
+
+        equal_df = spark.sql("select * from pointdf where ST_Equals(pointdf.point, ST_Point(100.1, 200.1)) ")
+        equal_df.show()
+
+        assert (equal_df.count() == 5, f"Expected 5 value but got ${equal_df.count()}")
