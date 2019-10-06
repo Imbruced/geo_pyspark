49c10e9125c006495fbcf24c9aee9e0c609e04b8
pawel93kocinski@gmail.com <pawel93kocinski@gmail.com>
2019-10-06 18:21:04 +0200

    Docstring for method registerAllin class GeoSparkRegistrator.

--- geo_pyspark/register/geo_registrator.py
+++ geo_pyspark/register/geo_registrator.py
@@ -11,6 +11,13 @@ class GeoSparkRegistrator:
 
     @classmethod
     def registerAll(cls, spark: SparkSession) -> bool:
+        """
+        This is the core of whole package, It uses py4j to run wrapper which takes existing SparkSession
+        and register all User Defined Functions by GeoSpark developers, for this SparkSession.
+
+        :param spark: pyspark.sql.SparkSession, spark session instance
+        :return: bool, True if registration was correct.
+        """
         spark.sql("SELECT 1 as geom").count()
         cls.register(spark)
         return True
