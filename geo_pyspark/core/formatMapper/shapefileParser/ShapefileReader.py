036ac9547bcbb8c0f6b82612a351a7a89910206d
Pawel <pawel93kocinski@gmail.com>
2019-11-13 23:00:10 +0100

    Added ShapefileReader class to handle reading from Shapefiles.

---
+++ geo_pyspark/core/formatMapper/shapefileParser/ShapefileReader.py
@@ -0,0 +1,26 @@
+import attr
+from pyspark import SparkContext
+
+from geo_pyspark.core.SpatialRDD.SpatialRDD import SpatialRDD
+
+
+@attr.s
+class ShapefileReader:
+
+    @classmethod
+    def readToGeometryRDD(cls, sc: SparkContext, shapefileInputLocation: str):
+        shape_reader = cls._create_jsrdd(sc)
+        jsrdd = shape_reader.readToGeometryRDD(
+                sc._jsc,
+                shapefileInputLocation
+        )
+        return SpatialRDD.from_srdd(sc, jsrdd)
+
+    @classmethod
+    def _create_jsrdd(cls, sc: SparkContext):
+        return sc._jvm.org. \
+            datasyslab. \
+            geospark. \
+            formatMapper. \
+            shapefileParser. \
+            ShapefileReader
