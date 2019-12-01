82f46fbe973ac888a61fe8af3b2e60093ccb1616
Pawel <pawel93kocinski@gmail.com>
2019-12-01 20:07:36 +0100

    Added path and config variables.

---
+++ tests/test_rdd.py
@@ -0,0 +1,43 @@
+import os
+
+from pyspark.sql import SparkSession
+from shapely.geometry import Point
+
+from geo_pyspark.core.enums import GridType, FileDataSplitter, IndexType
+from geo_pyspark.core.geom_types import Envelope
+from geo_pyspark.register import upload_jars
+
+upload_jars()
+
+spark = SparkSession.builder.\
+    master("local[*]").\
+    getOrCreate()
+
+resource_folder = "geo_pyspark/data"
+
+point_rdd_input_location = os.path.join(resource_folder, "arealm-small.csv")
+
+point_rdd_splitter = FileDataSplitter.CSV
+
+point_rdd_index_type = IndexType.RTREE
+point_rdd_num_partitions = 5
+point_rdd_offset = 1
+
+polygon_rdd_input_location = os.path.join(resource_folder, "primaryroads-polygon.csv")
+polygon_rdd_splitter = FileDataSplitter.CSV
+polygon_rdd_index_type = IndexType.RTREE
+polygon_rdd_num_partitions = 5
+polygon_rdd_start_offset = 0
+polygon_rdd_offset = 5
+
+
+knn_query_point = Point(-84.01, 34.01)
+
+range_query_window = Envelope(-90.01, -80.01, 30.01, 40.01)
+
+join_query_partitionin_type = GridType.QUADTREE
+each_query_loop_times = 1
+
+
+class TestSpatialRDD:
+    pass
