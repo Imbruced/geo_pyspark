fd621dc224c4d7e63c76f8807909a5ea89cb1850
Pawel <pawel93kocinski@gmail.com>
2019-12-01 21:08:22 +0100

    Added JoinQuery, RangeQuery, KNNQuery to module all attribute.

--- geo_pyspark/core/spatialOperator/__init__.py
+++ geo_pyspark/core/spatialOperator/__init__.py
@@ -1,5 +1,7 @@
-from .JoinQuery import JoinQuery
+from .join_query import JoinQuery
+from .range_query import RangeQuery
+from .knn_query import KNNQuery
 
 __all__ = [
-    "JoinQuery"
+    "JoinQuery", "RangeQuery", "KNNQuery"
 ]
