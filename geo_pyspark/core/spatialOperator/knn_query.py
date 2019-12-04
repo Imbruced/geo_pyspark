acc8d40b1037a085cf7e3497279df9cc2d31b7b5
Pawel <pawel93kocinski@gmail.com>
2019-12-04 23:42:30 +0100

    Added method SpatialKnnQuery as wrapper for SpatialKnnQuery in GeoSpark core.

--- geo_pyspark/core/spatialOperator/knn_query.py
+++ geo_pyspark/core/spatialOperator/knn_query.py
@@ -1,8 +1,19 @@
 import attr
 
+from geo_pyspark.core.SpatialRDD.abstract import AbstractSpatialRDD
+
 
 @attr.s
 class KNNQuery:
 
-    def SpatialKnnQuery(self):
-        pass
+    @classmethod
+    def SpatialKnnQuery(self, spatialRDD: AbstractSpatialRDD, originalQueryPoint, k: int,  useIndex: bool):
+        res = spatialRDD.sparkContext._jvm.\
+            org.datasyslab.geospark.spatialOperator.KNNQuery.SpatialKnnQuery(
+            spatialRDD._srdd,
+            originalQueryPoint,
+            k,
+            useIndex
+        )
+
+        return res
