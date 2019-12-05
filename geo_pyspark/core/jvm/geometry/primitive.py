96937be9260991c3853bf572f710266ebdac9622
Pawel <pawel93kocinski@gmail.com>
2019-12-05 23:54:08 +0100

    Added JvmPoint and JvmEnvelope classes.

--- geo_pyspark/core/jvm/geometry/primitive.py
+++ geo_pyspark/core/jvm/geometry/primitive.py
@@ -4,19 +4,6 @@ from geo_pyspark.core.jvm.abstract import JvmObject
 
 
 @attr.s
-class JvmPoint(JvmObject):
-    x = attr.ib(default=0.0)
-    y = attr.ib(default=0.0)
-
-    @property
-    def jvm_object(self):
-        return "com.vividsolutions.jts.geom.geometryFactory.createPoint"
-
-    def create_jvm_instance(self):
-        return "geometryFactory.createPoint(new Coordinate(-84.01, 34.01))"
-
-
-@attr.s
 class JvmCoordinates(JvmObject):
     x = attr.ib(default=0.0)
     y = attr.ib(default=0.0)
@@ -24,3 +11,32 @@ class JvmCoordinates(JvmObject):
     @property
     def jvm_reference(self):
         return "com.vividsolutions.jts.geom.Coordinate"
+
+
+@attr.s
+class JvmPoint(JvmObject):
+    coordinate = attr.ib(type=JvmCoordinates)
+
+    @property
+    def jvm_object(self):
+        return "com.vividsolutions.jts.geom.geometryFactory.createPoint"
+
+    def create_jvm_instance(self):
+
+        return "geometryFactory.createPoint(new Coordinate(-84.01, 34.01))"
+
+
+@attr.s
+class JvmEnvelope(JvmObject):
+    minx = attr.ib()
+    maxx = attr.ib()
+    miny = attr.ib()
+    maxy = attr.ib()
+
+    def create_jvm_instance(self):
+        envelope = getattr(self.jvm, self.jvm_reference)
+        return envelope(self.minx, self.maxx, self.miny, self.maxy)
+
+    @property
+    def jvm_reference(self):
+        return "com.vividsolutions.jts.geom.Envelope"
