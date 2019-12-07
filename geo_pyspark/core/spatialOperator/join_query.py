5a55b4694356b3edca0aea50cfcb40a8f34f089c
Pawel <pawel93kocinski@gmail.com>
2019-12-07 21:47:18 +0100

    Implemented DistanceJoinQuery for JoinQuery object.

--- geo_pyspark/core/spatialOperator/join_query.py
+++ geo_pyspark/core/spatialOperator/join_query.py
@@ -6,13 +6,7 @@ from geo_pyspark.core.jvm.abstract import JvmObject
 class JoinQuery:
 
     @classmethod
-    def SpatialJoinQuery(
-            cls,
-            spatialRDD,
-            queryRDD,
-            useIndex: bool,
-            considerBoundaryIntersection: bool
-    ):
+    def SpatialJoinQuery(cls, spatialRDD, queryRDD, useIndex: bool, considerBoundaryIntersection: bool):
         return JvmSpatialJoinQuery(
             spatialRDD._jvm,
             spatialRDD,
@@ -20,6 +14,16 @@ class JoinQuery:
             useIndex,
             considerBoundaryIntersection
         ).SpatialJoinQuery()
+
+    @classmethod
+    def DistanceJoinQuery(cls, spatialRDD, queryRDD, useIndex: bool, considerBoundaryIntersection: bool):
+        return JvmDistanceJoinQuery(
+            spatialRDD._jvm,
+            spatialRDD,
+            queryRDD,
+            useIndex,
+            considerBoundaryIntersection
+        ).DistanceJoinQuery()
 
 
 @attr.s
@@ -38,6 +42,31 @@ class JvmSpatialJoinQuery(JvmObject):
             SpatialJoinQuery
 
     def SpatialJoinQuery(self):
+        spatial_join = self.create_jvm_instance()
+        return spatial_join(
+            self.spatialRDD._srdd,
+            self.queryRDD._srdd,
+            self.useIndex,
+            self.considerBoundaryIntersection
+        )
+
+
+@attr.s
+class JvmDistanceJoinQuery(JvmObject):
+    spatialRDD = attr.ib()
+    queryRDD = attr.ib()
+    useIndex = attr.ib()
+    considerBoundaryIntersection = attr.ib()
+
+    def create_jvm_instance(self):
+        return self.jvm.org.\
+            datasyslab.\
+            geospark.\
+            spatialOperator.\
+            JoinQuery.\
+            DistanceJoinQuery
+
+    def DistanceJoinQuery(self):
         spatial_join = self.create_jvm_instance()
         return spatial_join(
             self.spatialRDD._srdd,
