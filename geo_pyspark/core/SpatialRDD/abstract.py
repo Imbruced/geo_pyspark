69582906f007bdba4da05cfd471f97567920c938
Pawel <pawel93kocinski@gmail.com>
2019-12-07 22:12:37 +0100

    Removed constructor attributes due to CircleRDD implementation, moved all attributes to SpatialRDD.

--- geo_pyspark/core/SpatialRDD/abstract.py
+++ geo_pyspark/core/SpatialRDD/abstract.py
@@ -13,25 +13,7 @@ from geo_pyspark.utils.types import crs, path
 @attr.s
 class AbstractSpatialRDD(ABC):
 
-    sparkContext = attr.ib(type=Optional[SparkContext], default=None)
-    InputLocation = attr.ib(type=Optional[path], default=None)
-    splitter = attr.ib(type=Optional[str], default=None)
-    carryInputData = attr.ib(type=Optional[bool], default=None)
-    partitions = attr.ib(type=Optional[int], default=None)
-    newLevel = attr.ib(type=Optional[str], default=None)
-    sourceEpsgCRSCode = attr.ib(type=crs, default=None)
-    targetEpsgCode = attr.ib(type=Optional[crs], default=None)
-
     def __attrs_post_init__(self):
-        if self.sparkContext is not None:
-            self._jsc = self.sparkContext._jsc
-            self._jvm = self.sparkContext._jvm
-            if self.splitter is not None:
-                self.__file_spliter_jvm = FileSplitterJvm(self.sparkContext)
-                self.splitter = self.__file_spliter_jvm.get_splitter(self.splitter)
-        else:
-            self._jsc = None
-            self._jvm = None
         self._srdd = self.srdd_from_attributes()
 
     def srdd_from_attributes(self):
