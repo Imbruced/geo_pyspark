2b2abd4ac296a99dca34b788ad524af30ca4fd13
Pawel <pawel93kocinski@gmail.com>
2019-12-07 19:09:43 +0100

    Simplified JvmObject.

--- geo_pyspark/core/jvm/abstract.py
+++ geo_pyspark/core/jvm/abstract.py
@@ -8,14 +8,6 @@ class JvmObject(ABC):
 
     jvm = attr.ib()
 
-    @property
-    def jvm_reference(self):
-        if self.jvm is None:
-            raise AttributeError("Jvm object can not be found")
-
-    def get_reference(self):
-        return getattr(self.jvm, self.jvm_reference)
-
     def create_jvm_instance(self):
         raise NotImplemented("Instance has to implement create_jvm_instance")
 
