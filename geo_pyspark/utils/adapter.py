964d7a2385887bb6b6017fe91b9557f3f530bbe6
Pawel <pawel93kocinski@gmail.com>
2019-12-09 21:29:35 +0100

    Added template for methods toDf and toRdd.

--- geo_pyspark/utils/adapter.py
+++ geo_pyspark/utils/adapter.py
@@ -1,6 +1,25 @@
+from typing import List
+
 import attr
+from pyspark.sql import DataFrame, SparkSession
+
+from geo_pyspark.core.SpatialRDD.abstract import AbstractSpatialRDD
 
 
 @attr.s
 class Adapter:
-    pass
+    """
+    Class which allow to convert between Spark DataFrame and SpatialRDD and reverse.
+    """
+
+    @classmethod
+    def toDf(cls, spatialRDD: AbstractSpatialRDD, spark: SparkSession, field_names: List[str]) -> DataFrame:
+        pass
+
+    @classmethod
+    def toRdd(cls, data_frame: DataFrame, geometry_col_id: int = None, geometry_field_name: str = None) -> DataFrame:
+        pass
+
+    @classmethod
+    def toSpatialRdd(cls) -> AbstractSpatialRDD:
+        pass
