c24d8f15d3a74538c74bd0eae5a6da24add69417
Pawel <pawel93kocinski@gmail.com>
2019-12-16 22:53:56 +0100

    Added Serializer for geospark data.

--- geo_pyspark/utils/serde.py
+++ geo_pyspark/utils/serde.py
@@ -1,5 +1,7 @@
 from abc import ABC
 
+from pyspark import PickleSerializer
+
 from geo_pyspark.utils.decorators import classproperty
 
 
@@ -22,3 +24,12 @@ class GeoSparkKryoRegistrator(Serializer):
     @classproperty
     def getName(self):
         return "org.datasyslab.geospark.serde.GeoSparkKryoRegistrator"
+
+
+class GeoSparkPickler(PickleSerializer):
+
+    def loads(self, obj, encoding="bytes"):
+        return [el for el in obj]
+
+    def dumps(self, obj):
+        return super().dumps(obj)
