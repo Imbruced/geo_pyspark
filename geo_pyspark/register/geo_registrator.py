973610f99eb4f8eb8a8bd6d1adb168cfe39753e0
Pawel <pawel93kocinski@gmail.com>
2019-12-17 18:57:30 +0100

    Imported all libraries in GeoSparkRegistrator class.

--- geo_pyspark/register/geo_registrator.py
+++ geo_pyspark/register/geo_registrator.py
@@ -1,9 +1,14 @@
+from typing import List
+
 import attr
 from pyspark.sql import SparkSession
+from py4j.java_gateway import java_import
 
+from geo_pyspark.register.java_libs import GEOSPARK_IMPORTS
 from geo_pyspark.utils.prep import assign_all
 
 assign_all()
+jvm_import = str
 
 
 @attr.s
@@ -19,12 +24,13 @@ class GeoSparkRegistrator:
         :return: bool, True if registration was correct.
         """
         spark.sql("SELECT 1 as geom").count()
+        PackageImporter.import_jvm_lib(spark._jvm, GEOSPARK_IMPORTS)
         cls.register(spark)
         return True
 
     @classmethod
     def register(cls, spark: SparkSession):
-        spark._jvm.registerAll()
+        spark._jvm.GeoSparkWrapper.registerAll()
 
 
 class PackageImporter:
