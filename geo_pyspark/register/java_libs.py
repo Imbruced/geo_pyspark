9923244b1dfe77909fc9c0152117f1db0f54f18c
Pawel <pawel93kocinski@gmail.com>
2019-12-17 18:57:50 +0100

    Added used geospark libraries.

---
+++ geo_pyspark/register/java_libs.py
@@ -0,0 +1,11 @@
+GEOSPARK_IMPORTS = [
+    "org.imbruced.geo_pyspark.JoinParams",
+    "org.datasyslab.geosparksql.utils.Adapter",
+    "org.imbruced.geo_pyspark.GeoSparkWrapper",
+    "org.datasyslab.geospark.spatialOperator.JoinQuery"
+    "org.datasyslab.geospark.spatialOperator.KNNQuery",
+    "org.imbruced.geo_pyspark.CoordinateFactory",
+    "org.datasyslab.geospark.spatialOperator.RangeQuery",
+    "org.imbruced.geo_pyspark.GeomFactory",
+    "com.vividsolutions.jts.geom.Envelope"
+]
