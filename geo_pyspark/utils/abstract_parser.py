d64f541a95bc06b802b976c4fc388a8ecfa880ef
Pawel <pawel93kocinski@gmail.com>
2019-12-25 12:43:17 +0100

    Added abstract parser for SpatialRDD.

--- geo_pyspark/utils/abstract_parser.py
+++ geo_pyspark/utils/abstract_parser.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from typing import List, Any
 
 import attr
 from shapely.geometry.base import BaseGeometry
@@ -16,6 +17,18 @@ class GeometryParser(ABC):
     @classmethod
     def serialize(cls, obj: BaseGeometry, binary_buffer: BinaryBuffer):
         raise NotImplementedError("Parser has to implement serialize method")
+
+    @classmethod
+    def deserialize(cls, bin_parser: BinaryParser) -> BaseGeometry:
+        raise NotImplementedError("Parser has to implement deserialize method")
+
+
+@attr.s
+class AbstractSpatialRDDParser(ABC):
+
+    @classmethod
+    def serialize(cls, obj: List[Any], binary_buffer: BinaryBuffer) -> bytearray:
+        raise NotImplemented()
 
     @classmethod
     def deserialize(cls, bin_parser: BinaryParser) -> BaseGeometry:
