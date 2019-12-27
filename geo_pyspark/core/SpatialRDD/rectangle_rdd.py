f8a9c5a1ad064147908057100e7181634bee34f6
Pawel <pawel93kocinski@gmail.com>
2019-12-27 17:19:15 +0100

    Added RectangleRDD.

--- geo_pyspark/core/SpatialRDD/rectangle_rdd.py
+++ geo_pyspark/core/SpatialRDD/rectangle_rdd.py
@@ -1,14 +1,35 @@
+from typing import Optional
+
 import attr
 
 from geo_pyspark.core.SpatialRDD.spatial_rdd import SpatialRDD
+from geo_pyspark.core.SpatialRDD.spatial_rdd_factory import SpatialRDDFactory
 
 
 @attr.s
 class RectangleRDD(SpatialRDD):
+    Offset = attr.ib(default=None, type=Optional[int])
 
     def __attrs_post_init__(self):
+        from geo_pyspark.core.SpatialRDD import PolygonRDD
         super().__attrs_post_init__()
-        self._srdd = self.__create_srdd()
+        arguments = self.sparkContext, self.InputLocation, self.Offset, self.splitter, self.carryInputData, self.partitions
+        arguments_is_none = [arg is not None for arg in arguments]
 
-    def __create_srdd(self):
-        pass
+        if isinstance(self.spatialRDD, PolygonRDD):
+            self._srdd = self.spatialRDD._srdd.MinimumBoundingRectangle()
+        elif all(arguments_is_none):
+            rectangle_rdd = SpatialRDDFactory(self.sparkContext).\
+                create_rectangle_rdd()
+            j_rdd = rectangle_rdd(
+                    self._jsc,
+                    self.InputLocation,
+                    self.Offset,
+                    self.splitter,
+                    self.carryInputData,
+                    self.partitions
+            )
+
+            self._srdd = j_rdd
+        else:
+            self._srdd = None
