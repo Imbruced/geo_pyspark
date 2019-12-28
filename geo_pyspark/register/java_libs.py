251f8864911a90e071e9d68a3b96f5fd64c546a1
Pawel <pawel93kocinski@gmail.com>
2019-12-28 21:53:39 +0100

    Changed list of java libraries to Enumeration.

--- geo_pyspark/register/java_libs.py
+++ geo_pyspark/register/java_libs.py
@@ -1,19 +1,22 @@
-GEOSPARK_IMPORTS = [
-    "org.imbruced.geo_pyspark.JoinParams",
-    "org.datasyslab.geosparksql.utils.Adapter",
-    "org.imbruced.geo_pyspark.GeoSparkWrapper",
-    "org.datasyslab.geospark.spatialOperator.JoinQuery"
-    "org.datasyslab.geospark.spatialOperator.KNNQuery",
-    "org.imbruced.geo_pyspark.CoordinateFactory",
-    "org.datasyslab.geospark.spatialOperator.RangeQuery",
-    "org.imbruced.geo_pyspark.GeomFactory",
-    "com.vividsolutions.jts.geom.Envelope",
-    "org.imbruced.geo_pyspark.serializers.GeoSerializerNoUserAttributes",
-    "org.imbruced.geo_pyspark.serializers.GeoSerializerData",
-    "org.datasyslab.geospark.spatialRDD.PointRDD",
-    "org.datasyslab.geospark.spatialRDD.PolygonRDD",
-    "org.datasyslab.geospark.spatialRDD.CircleRDD",
-    "org.datasyslab.geospark.spatialRDD.LineStringRDD",
-    "org.datasyslab.geospark.spatialRDD.RectangleRDD",
-    "org.datasyslab.geospark.enums.FileDataSplitter"
-]
+from enum import Enum
+
+
+class GeoSparkLib(Enum):
+    JoinParams = "org.imbruced.geo_pyspark.JoinParams"
+    Adapter = "org.datasyslab.geosparksql.utils.Adapter"
+    GeoSparkWrapper = "org.imbruced.geo_pyspark.GeoSparkWrapper"
+    JoinQuery = "org.datasyslab.geospark.spatialOperator.JoinQuery"
+    KNNQuery = "org.datasyslab.geospark.spatialOperator.KNNQuery"
+    CoordinateFactory = "org.imbruced.geo_pyspark.CoordinateFactory"
+    RangeQuery = "org.datasyslab.geospark.spatialOperator.RangeQuery"
+    GeomFactory = "org.imbruced.geo_pyspark.GeomFactory"
+    Envelope = "com.vividsolutions.jts.geom.Envelope"
+    GeoSerializerData = "org.imbruced.geo_pyspark.serializers.GeoSerializerData"
+    PointRDD = "org.datasyslab.geospark.spatialRDD.PointRDD"
+    PolygonRDD = "org.datasyslab.geospark.spatialRDD.PolygonRDD"
+    CircleRDD = "org.datasyslab.geospark.spatialRDD.CircleRDD"
+    LineStringRDD = "org.datasyslab.geospark.spatialRDD.LineStringRDD"
+    RectangleRDD = "org.datasyslab.geospark.spatialRDD.RectangleRDD"
+    FileDataSplitter = "org.datasyslab.geospark.enums.FileDataSplitter"
+    GeoJsonReader = "org.datasyslab.geospark.formatMapper.GeoJsonReader"
+    ShapeFileReader = "org.datasyslab.geospark.formatMapper.shapefileParser.ShapefileReader"
