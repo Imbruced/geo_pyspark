d2ced463f4deccc8798e28c909541134d07ac205
Pawel <pawel93kocinski@gmail.com>
2019-12-30 23:22:39 +0100

    Added type of RDD on first byte.

--- geo_pyspark/utils/serde.py
+++ geo_pyspark/utils/serde.py
@@ -3,13 +3,14 @@ from abc import ABC
 import attr
 from pyspark import PickleSerializer
 
-from geo_pyspark.utils.abstract_parser import GeometryParser
 from geo_pyspark.utils.binary_parser import BinaryParser
 from geo_pyspark.utils.decorators import classproperty
 from geo_pyspark.utils.spatial_rdd_parser import SpatialPairRDDParserNonUserData
 
 PARSERS = {
     0: SpatialPairRDDParserNonUserData(),
+    1: SpatialPairRDDParserNonUserData(),
+    2: SpatialPairRDDParserNonUserData(),
 
 
 }
@@ -53,7 +54,7 @@ class GeoSparkPickler(PickleSerializer):
 
     def loads(self, obj, encoding="bytes"):
         binary_parser = BinaryParser(obj)
-        spatial_parser_number = 0
+        spatial_parser_number = binary_parser.read_int()
         spatial_parser = self.get_parser(spatial_parser_number)
         parsed_row = spatial_parser.deserialize(binary_parser)
 
