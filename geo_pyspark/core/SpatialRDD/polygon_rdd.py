67a462dfe83d4cc0cebffaa65838ecaf830e5285
Pawel <pawel93kocinski@gmail.com>
2019-12-31 18:04:13 +0100

    Implemented constructors for PolygonRDD with metaclass MultipleMeta.

--- geo_pyspark/core/SpatialRDD/polygon_rdd.py
+++ geo_pyspark/core/SpatialRDD/polygon_rdd.py
@@ -1,52 +1,399 @@
-from typing import Optional
+from pyspark import SparkContext, StorageLevel
 
-import attr
-from pyspark import SparkContext
-
-from geo_pyspark.core.SpatialRDD.spatial_rdd import SpatialRDD
+from geo_pyspark.core.SpatialRDD.spatial_rdd import SpatialRDD, MultipleMeta
 from geo_pyspark.core.SpatialRDD.spatial_rdd_factory import SpatialRDDFactory
-from geo_pyspark.core.enums.file_data_splitter import FileSplitterJvm
-from geo_pyspark.utils.types import path, crs
+from geo_pyspark.core.enums.file_data_splitter import FileSplitterJvm, FileDataSplitter
 
 
-@attr.s
-class PolygonRDD(SpatialRDD):
-    sparkContext = attr.ib(type=Optional[SparkContext], default=None)
-    InputLocation = attr.ib(type=Optional[path], default=None)
-    splitter = attr.ib(type=Optional[str], default=None)
-    carryInputData = attr.ib(type=Optional[bool], default=None)
-    partitions = attr.ib(type=Optional[int], default=None)
-    newLevel = attr.ib(type=Optional[str], default=None)
-    sourceEpsgCRSCode = attr.ib(type=crs, default=None)
-    targetEpsgCode = attr.ib(type=Optional[crs], default=None)
-    spatialRDD = attr.ib(type=Optional['SpatialRDD'], default=None)
-    startingOffset = attr.ib(default=None, type=Optional[int])
-    endingOffset = attr.ib(default=None, type=Optional[int])
+class PolygonRDD(SpatialRDD, metaclass=MultipleMeta):
 
-    def __attrs_post_init__(self):
-        if self.spatialRDD is not None:
-            self._srdd = self.spatialRDD._srdd
-        if self.splitter is not None and self.sparkContext is not None:
-            self._jvm_splitter = FileSplitterJvm(self.sparkContext._jvm, self.splitter).jvm_instance
-        super().__attrs_post_init__()
-        if all([self.sparkContext]):
-            PolygonRDD = SpatialRDDFactory(self.sparkContext).create_polygon_rdd()
+    def __init__(self):
+        super().__init__()
 
-            self._srdd = PolygonRDD(
-                self._jsc,
-                self.InputLocation,
-                self.startingOffset,
-                self.endingOffset,
-                self._jvm_splitter,
-                self.carryInputData
-            )
-        else:
-            self._srdd = None
+    def __init__(self, rawSpatialRDD: SpatialRDD):
+        """
+
+        :param rawSpatialRDD:
+        """
+        super().__init__(rawSpatialRDD._sc)
+        self._srdd = rawSpatialRDD._srdd
+
+    def __init__(self, rawSpatialRDD: SpatialRDD, sourceEpsgCode: str, targetEpsgCode: str):
+        """
+
+        :param rawSpatialRDD:
+        :param sourceEpsgCode: str, the source epsg CRS code
+        :param targetEpsgCode: str, the target epsg code
+        """
+        super().__init__(rawSpatialRDD._sc)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(rawSpatialRDD._sc)
+
+        self._srdd = jvm_polygon_rdd(
+            rawSpatialRDD._srdd.rawSpatialRDD(),
+            sourceEpsgCode,
+            targetEpsgCode
+        )
+
+    def __init__(self, rawSpatialRDD: SpatialRDD, newLevel: StorageLevel):
+        """
+
+        :param rawSpatialRDD:
+        :param newLevel:
+        """
+        super().__init__(rawSpatialRDD._sc)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(rawSpatialRDD._sc)
+        self._srdd = jvm_polygon_rdd(
+            rawSpatialRDD._srdd.rawSpatialRDD(),
+            newLevel
+        )
+
+    def __init__(self, sparkContext: SparkContext, InputLocation: str, startOffset: int, endOffset: int,
+                 splitter: FileDataSplitter, carryInputData: bool, partitions: int):
+        """
+
+        :param sparkContext: SparkContext, the spark context
+        :param InputLocation: str, the input location
+        :param startOffset:
+        :param endOffset:
+        :param splitter: FileDataSplitter, File data splitter which should be used to split the data
+        :param carryInputData:
+        :param partitions: int, the partitions
+        """
+        super().__init__(sparkContext)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+        jvm_splitter = FileSplitterJvm(self._jvm, splitter)
+
+        self._srdd = jvm_polygon_rdd(
+            self._jsc,
+            InputLocation,
+            startOffset,
+            endOffset,
+            jvm_splitter.jvm_instance,
+            carryInputData,
+            partitions
+        )
+
+    def __init__(self, sparkContext: SparkContext, InputLocation: str, startOffset: int, endOffset: int,
+                 splitter: FileDataSplitter, carryInputData: bool):
+        """
+
+        :param sparkContext: SparkContext, the spark context
+        :param InputLocation: str, the input location
+        :param startOffset:
+        :param endOffset:
+        :param splitter: FileDataSplitter, File data splitter which should be used to split the data
+        :param carryInputData:
+        """
+        super().__init__(sparkContext)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+        jvm_splitter = FileSplitterJvm(self._jvm, splitter)
+
+        self._srdd = jvm_polygon_rdd(
+            self._jsc,
+            InputLocation,
+            startOffset,
+            endOffset,
+            jvm_splitter.jvm_instance,
+            carryInputData
+        )
+
+    def __init__(self, sparkContext: SparkContext, InputLocation: str, splitter: FileDataSplitter,
+                 carryInputData: bool, partitions: int):
+        """
+
+        :param sparkContext: SparkContext, the spark context
+        :param InputLocation: str, the input location
+        :param splitter: FileDataSplitter, File data splitter which should be used to split the data
+        :param carryInputData:
+        :param partitions: int, the partitions
+        """
+
+        super().__init__(sparkContext)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+        jvm_splitter = FileSplitterJvm(self._jvm, splitter)
+
+        self._srdd = jvm_polygon_rdd(
+            self._jsc,
+            InputLocation,
+            jvm_splitter.jvm_instance,
+            carryInputData,
+            partitions
+        )
+
+    def __init__(self, sparkContext: SparkContext, InputLocation: str, splitter: FileDataSplitter, carryInputData: bool):
+        """
+
+        :param sparkContext: SparkContext, the spark context
+        :param InputLocation: str, the input location
+        :param splitter: FileDataSplitter, File data splitter which should be used to split the data
+        :param carryInputData:
+        """
+
+        super().__init__(sparkContext)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+        jvm_splitter = FileSplitterJvm(self._jvm, splitter)
+
+        self._srdd = jvm_polygon_rdd(
+            self._jsc,
+            InputLocation,
+            jvm_splitter.jvm_instance,
+            carryInputData
+        )
+
+    def __init__(self, sparkContext: SparkContext, InputLocation: str, startOffset: int, endOffset: int,
+            splitter: FileDataSplitter, carryInputData: bool, partitions: int, newLevel: StorageLevel):
+        """
+
+        :param sparkContext: SparkContext, the spark context
+        :param InputLocation: str, the input location
+        :param startOffset:
+        :param endOffset:
+        :param splitter: FileDataSplitter, File data splitter which should be used to split the data
+        :param carryInputData:
+        :param partitions: int, the partitions
+        :param newLevel:
+        """
+        super().__init__(sparkContext)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+        jvm_splitter = FileSplitterJvm(self._jvm, splitter)
+
+        self._srdd = jvm_polygon_rdd(
+            self._jsc,
+            InputLocation,
+            startOffset,
+            endOffset,
+            jvm_splitter.jvm_instance,
+            carryInputData,
+            partitions,
+            newLevel
+        )
+
+    def __init__(self, sparkContext: SparkContext, InputLocation: str, startOffset: int, endOffset: int,
+            splitter: FileDataSplitter, carryInputData: bool, newLevel: StorageLevel):
+        """
+
+        :param sparkContext: SparkContext, the spark context
+        :param InputLocation: str, the input location
+        :param startOffset:
+        :param endOffset:
+        :param splitter: FileDataSplitter, File data splitter which should be used to split the data
+        :param carryInputData:
+        :param newLevel:
+        """
+
+        super().__init__(sparkContext)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+        jvm_splitter = FileSplitterJvm(self._jvm, splitter)
+
+        self._srdd = jvm_polygon_rdd(
+            self._jsc,
+            InputLocation,
+            startOffset,
+            endOffset,
+            jvm_splitter.jvm_instance,
+            carryInputData,
+            newLevel
+        )
+
+    def __init__(self, sparkContext: SparkContext, InputLocation: str,
+            splitter: FileDataSplitter, carryInputData: bool, partitions: int, newLevel: StorageLevel):
+        """
+
+        :param sparkContext: SparkContext, the spark context
+        :param InputLocation: str, the input location
+        :param splitter: FileDataSplitter, File data splitter which should be used to split the data
+        :param carryInputData:
+        :param partitions: int, the partitions
+        :param newLevel:
+        """
+
+        super().__init__(sparkContext)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+        jvm_splitter = FileSplitterJvm(self._jvm, splitter)
+
+        self._srdd = jvm_polygon_rdd(
+            self._jsc,
+            InputLocation,
+            jvm_splitter.jvm_instance,
+            carryInputData,
+            partitions,
+            newLevel
+        )
+
+    def __init__(self, sparkContext: SparkContext, InputLocation: str,
+            splitter: FileDataSplitter, carryInputData: bool, newLevel: StorageLevel):
+        """
+
+        :param sparkContext: SparkContext, the spark context
+        :param InputLocation: str, the input location
+        :param splitter: FileDataSplitter, File data splitter which should be used to split the data
+        :param carryInputData:
+        :param newLevel:
+        """
+        super().__init__(sparkContext)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+        jvm_splitter = FileSplitterJvm(self._jvm, splitter)
+
+        self._srdd = jvm_polygon_rdd(
+            self._jsc,
+            InputLocation,
+            jvm_splitter.jvm_instance,
+            carryInputData,
+            newLevel
+        )
+
+    def __init__(self, rawSpatialRDD: SpatialRDD, newLevel: StorageLevel, sourceEpsgCRSCode: str, targetEpsgCode: str):
+        """
+
+        :param rawSpatialRDD:
+        :param newLevel:
+        :param sourceEpsgCRSCode: str, the source epsg CRS code
+        :param targetEpsgCode: str, the target epsg code
+        """
+
+        super().__init__(rawSpatialRDD._sc)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+
+        self._srdd = jvm_polygon_rdd(
+            rawSpatialRDD._srdd.rawSpatialRDD(),
+            newLevel,
+            sourceEpsgCRSCode,
+            targetEpsgCode
+        )
+
+    def __init__(self, sparkContext: SparkContext, InputLocation: str, startOffset: int, endOffset: str,
+            splitter: FileDataSplitter, carryInputData: bool, partitions: int, newLevel: StorageLevel,
+                 sourceEpsgCRSCode: str, targetEpsgCode: str):
+        """
+
+        :param sparkContext: SparkContext, the spark context
+        :param InputLocation: str, the input location
+        :param startOffset:
+        :param endOffset:
+        :param splitter: FileDataSplitter, File data splitter which should be used to split the data
+        :param carryInputData:
+        :param partitions: int, the partitions
+        :param newLevel:
+        :param sourceEpsgCRSCode: str, the source epsg CRS code
+        :param targetEpsgCode: str, the target epsg code
+        """
+
+        super().__init__(sparkContext)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+        jvm_splitter = FileSplitterJvm(self._jvm, splitter)
+
+        self._srdd = jvm_polygon_rdd(
+            self._jsc,
+            InputLocation,
+            startOffset,
+            endOffset,
+            jvm_splitter.jvm_instance,
+            carryInputData,
+            partitions,
+            newLevel,
+            sourceEpsgCRSCode,
+            targetEpsgCode
+        )
+
+
+
+    def __init__(self, sparkContext: SparkContext, InputLocation: str, startOffset: int, endOffset: int,
+            splitter: FileDataSplitter, carryInputData: bool, newLevel: StorageLevel, sourceEpsgCRSCode: str,
+            targetEpsgCode: str):
+        """
+
+        :param sparkContext: SparkContext, the spark context
+        :param InputLocation: str, the input location
+        :param startOffset:
+        :param endOffset:
+        :param splitter: FileDataSplitter, File data splitter which should be used to split the data
+        :param carryInputData:
+        :param newLevel:
+        :param sourceEpsgCRSCode: str, the source epsg CRS code
+        :param targetEpsgCode: str, the target epsg code
+        """
+        super().__init__(sparkContext)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+        jvm_splitter = FileSplitterJvm(self._jvm, splitter)
+
+        self._srdd = jvm_polygon_rdd(
+            self._jsc,
+            InputLocation,
+            startOffset,
+            endOffset,
+            jvm_splitter.jvm_instance,
+            carryInputData,
+            newLevel,
+            sourceEpsgCRSCode,
+            targetEpsgCode
+        )
+
+    def __init__(self, sparkContext: SparkContext, InputLocation: str, splitter: FileDataSplitter, carryInputData: bool,
+                 partitions: int, newLevel: StorageLevel, sourceEpsgCRSCode: str,  targetEpsgCode: str):
+        """
+
+        :param sparkContext: SparkContext, the spark context
+        :param InputLocation: str, the input location
+        :param splitter: FileDataSplitter, File data splitter which should be used to split the data
+        :param carryInputData:
+        :param partitions: int, the partitions
+        :param newLevel:
+        :param sourceEpsgCRSCode: str, the source epsg CRS code
+        :param targetEpsgCode: str, the target epsg code
+        """
+        super().__init__(sparkContext)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+        jvm_splitter = FileSplitterJvm(self._jvm, splitter)
+
+        self._srdd = jvm_polygon_rdd(
+            self._jsc,
+            InputLocation,
+            jvm_splitter.jvm_instance,
+            carryInputData,
+            partitions,
+            newLevel,
+            sourceEpsgCRSCode,
+            targetEpsgCode
+        )
+
+    def __init__(self, sparkContext: SparkContext, InputLocation: str, splitter: FileDataSplitter,
+                 carryInputData: bool, newLevel: StorageLevel, sourceEpsgCRSCode: str, targetEpsgCode: str):
+        """
+
+        :param sparkContext: SparkContext, the spark context
+        :param InputLocation: str, the input location
+        :param splitter: FileDataSplitter, File data splitter which should be used to split the data
+        :param carryInputData: bool,
+        :param newLevel:
+        :param sourceEpsgCRSCode: str, the source epsg CRS code
+        :param targetEpsgCode: str, the target epsg code
+        """
+        super().__init__(sparkContext)
+        jvm_polygon_rdd = self._create_jvm_polygon_rdd(self._sc)
+        jvm_splitter = FileSplitterJvm(self._jvm, splitter)
+
+        self._srdd = jvm_polygon_rdd(
+            self._jsc,
+            InputLocation,
+            jvm_splitter.jvm_instance,
+            carryInputData,
+            newLevel,
+            sourceEpsgCRSCode,
+            targetEpsgCode
+        )
 
     def MinimumBoundingRectangle(self):
         from geo_pyspark.core.SpatialRDD import RectangleRDD
         rectangle_rdd = RectangleRDD(
             spatialRDD=self,
-            sparkContext=self.sparkContext
+            sparkContext=self._sc
         )
         return rectangle_rdd
+
+    @staticmethod
+    def _create_jvm_polygon_rdd(sc: SparkContext):
+        spatial_factory = SpatialRDDFactory(sc)
+
+        jvm_polygon_rdd = spatial_factory.create_polygon_rdd()
+
+        return jvm_polygon_rdd
