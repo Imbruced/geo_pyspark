290ae69e1403c615cbf2d31263d3df9171308bb8
Pawel <pawel93kocinski@gmail.com>
2020-01-04 00:34:18 +0100

    Implemented GeoJsonReader and GeoReader with MultipleMeta.

--- geo_pyspark/core/formatMapper/geo_json_reader.py
+++ geo_pyspark/core/formatMapper/geo_json_reader.py
@@ -1,77 +1,95 @@
-from typing import Optional
+import inspect
 
-import attr
 from pyspark import SparkContext, RDD
 
 from geo_pyspark.core.SpatialRDD.spatial_rdd import SpatialRDD
 from geo_pyspark.core.formatMapper.geo_reader import GeoDataReader
-from geo_pyspark.core.jvm.abstract import JvmObject
 from geo_pyspark.core.utils import require
-from geo_pyspark.exceptions import InvalidParametersException
 from geo_pyspark.register.java_libs import GeoSparkLib
+from geo_pyspark.utils.meta import MultipleMeta
 
 
-class GeoJsonReader(GeoDataReader):
+class GeoJsonReader(GeoDataReader, metaclass=MultipleMeta):
 
     @classmethod
-    @require([GeoSparkLib.GeoJsonReader])
-    def readToGeometryRDD(cls,
-                          sparkContext: Optional[SparkContext] = None,
-                          inputPath: Optional[str] = None,
-                          allowInvalidGeometries: Optional[bool] = True,
-                          skipSyntaticallyInvalidGeometries: Optional[bool] = False,
-                          rawTextRDD: Optional[RDD] = None) -> SpatialRDD:
+    def validate_imports(cls) -> bool:
+        return True
+
+    @classmethod
+    def readToGeometryRDD(cls, sc: SparkContext, inputPath: str):
         """
-        :param sparkContext:
+
+        :param sc:
+        :param inputPath:
+        :return:
+        """
+        GeoJsonReader.validate_imports()
+        jvm = sc._jvm
+        srdd = jvm.GeoJsonReader.readToGeometryRDD(
+            sc._jsc, inputPath
+        )
+
+        spatial_rdd = SpatialRDD(sc)
+        spatial_rdd.set_srdd(srdd)
+        return spatial_rdd
+
+    @classmethod
+    def readToGeometryRDD(cls, sc: SparkContext, inputPath: str, allowInvalidGeometries: bool,
+                          skipSyntacticallyInvalidGeometries: bool) -> SpatialRDD:
+        """
+
+        :param sc:
         :param inputPath:
         :param allowInvalidGeometries:
-        :param skipSyntaticallyInvalidGeometries:
+        :param skipSyntacticallyInvalidGeometries:
+        :return:
+        """
+        GeoJsonReader.validate_imports()
+        jvm = sc._jvm
+        srdd = jvm.GeoJsonReader.readToGeometryRDD(
+            sc._jsc, inputPath, allowInvalidGeometries, skipSyntacticallyInvalidGeometries
+        )
+
+        spatial_rdd = SpatialRDD(sc)
+        spatial_rdd.set_srdd(srdd)
+        return spatial_rdd
+
+    @classmethod
+    def readToGeometryRDD(cls, rawTextRDD: RDD):
+        """
+
         :param rawTextRDD:
         :return:
         """
+        GeoJsonReader.validate_imports()
+        sc = rawTextRDD.ctx
+        jvm = sc._jvm
 
-        jvm_geo_json_reader = JvmGeoJsonReader(
-            jvm=sparkContext._jvm,
-            sparkContext=sparkContext,
-            inputPath=inputPath,
-            allowInvalidGeometries=allowInvalidGeometries,
-            skipSyntacticallyInvalidGeometries=skipSyntaticallyInvalidGeometries,
-            rawTextRDD=rawTextRDD
+        srdd = jvm.GeoJsonReader.readToGeometryRDD(
+            rawTextRDD._jrdd
         )
-        return jvm_geo_json_reader.readToGeometryRDD()
 
+        spatial_rdd = SpatialRDD(sc)
+        spatial_rdd.set_srdd(srdd)
+        return spatial_rdd
 
-@attr.s
-class JvmGeoJsonReader(JvmObject):
-    sparkContext = attr.ib(type=SparkContext, default=None)
-    inputPath = attr.ib(type=str, default=None)
-    allowInvalidGeometries = attr.ib(type=bool, default=None)
-    skipSyntacticallyInvalidGeometries = attr.ib(type=bool, default=None)
-    rawTextRDD = attr.ib(type=RDD, default=None)
+    @classmethod
+    def readToGeometryRDD(cls, rawTextRDD: RDD, allowInvalidGeometries: bool, skipSyntacticallyInvalidGeometries: bool):
+        """
 
-    def readToGeometryRDD(self) -> SpatialRDD:
-        if self.sparkContext is not None and self.inputPath is not None:
-            jvm = self.sparkContext._jvm
-            srdd = jvm.GeoJsonReader.readToGeometryRDD(
-                self.sparkContext._jsc,
-                self.inputPath,
-                self.allowInvalidGeometries,
-                self.skipSyntacticallyInvalidGeometries
-            )
+        :param rawTextRDD:
+        :param allowInvalidGeometries:
+        :param skipSyntacticallyInvalidGeometries:
+        :return:
+        """
+        GeoJsonReader.validate_imports()
+        sc = rawTextRDD.ctx
+        jvm = sc._jvm
 
-            spatial_rdd = SpatialRDD(sparkContext=self.sparkContext)
-            spatial_rdd._srdd = srdd
-            return spatial_rdd
+        srdd = jvm.GeoJsonReader.readToGeometryRDD(
+            rawTextRDD._jrdd, allowInvalidGeometries, skipSyntacticallyInvalidGeometries
+        )
 
-        elif self.sparkContext is None and self.inputPath is None and self.rawTextRDD is not None:
-            jvm = self.rawTextRDD.ctx._jvm
-            srdd = jvm.GeoJsonReader.readToGeometryRDD(
-                self.rawTextRDD._jrdd
-            )
-
-            spatial_rdd = SpatialRDD(sparkContext=self.rawTextRDD.ctx)
-            spatial_rdd._srdd = srdd
-            return spatial_rdd
-
-        else:
-            raise InvalidParametersException("Parsed parameters are incorrect")
+        spatial_rdd = SpatialRDD(sc)
+        spatial_rdd.set_srdd(srdd)
+        return spatial_rdd
--- geo_pyspark/core/formatMapper/geo_reader.py
+++ geo_pyspark/core/formatMapper/geo_reader.py
@@ -4,10 +4,16 @@ import abc
 import attr
 from pyspark import SparkContext
 
+from geo_pyspark.utils.meta import MultipleMeta
+
 
 @attr.s
-class GeoDataReader(ABC):
+class GeoDataReader(metaclass=MultipleMeta):
 
     @abc.abstractmethod
-    def readToGeometryRDD(cls):
+    def validate_imports(self):
+        pass
+
+    @abc.abstractmethod
+    def readToGeometryRDD(cls, *args, **kwargs):
         raise NotImplementedError(f"Instance of the class {cls.__class__.__name__} has to implement method readToGeometryRDD")
