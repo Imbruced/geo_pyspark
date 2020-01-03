71d0b432e0c4d8afbacdbad02ffac63dbb871c5b
Pawel <pawel93kocinski@gmail.com>
2020-01-04 00:32:36 +0100

    Changed from class property to Enumeration.

--- geo_pyspark/core/enums/file_data_splitter.py
+++ geo_pyspark/core/enums/file_data_splitter.py
@@ -1,88 +1,41 @@
+from enum import Enum
+
 import attr
-from pyspark import SparkContext
 
 from geo_pyspark.core.jvm.abstract import JvmObject
 from geo_pyspark.core.utils import require
 from geo_pyspark.register.java_libs import GeoSparkLib
-from geo_pyspark.utils.decorators import classproperty
 
 
-class FileDataSplitter:
+class FileDataSplitter(Enum):
 
-    @classproperty
-    def CSV(self):
-        return "CSV"
-
-    @classproperty
-    def TSV(self):
-        return "TSV"
-
-    @classproperty
-    def GEOJSON(self):
-        return "GEOJSON"
-
-    @classproperty
-    def WKT(self):
-        return "WKT"
-
-    @classproperty
-    def WKB(self):
-        return "WKB"
-
-    @classproperty
-    def COMMA(self):
-        return "COMMA"
-
-    @classproperty
-    def TAB(self):
-        return "TAB"
-
-    @classproperty
-    def QUESTIONMARK(self):
-        return "QUESTIONMARK"
-
-    @classproperty
-    def SINGLEQUOTE(self):
-        return "SINGLEQUOTE"
-
-    @classproperty
-    def QUOTE(self):
-        return "QUOTE"
-
-    @classproperty
-    def UNDERSCORE(self):
-        return "UNDERSCORE"
-
-    @classproperty
-    def DASH(self):
-        return "DASH"
-
-    @classproperty
-    def PERCENT(self):
-        return "PERCENT"
-
-    @classproperty
-    def TILDE(self):
-        return "TILDE"
-
-    @classproperty
-    def PIPE(self):
-        return "PIPE"
-
-    @classproperty
-    def SEMICOLON(self):
-        return "SEMICOLON"
+    CSV = "CSV"
+    TSV = "TSV"
+    GEOJSON = "GEOJSON"
+    WKT = "WKT"
+    WKB = "WKB"
+    COMA = "COMA"
+    TAB = "TAB"
+    QUESTIONMARK = "QUESTIONMARK"
+    SINGLEQUOTE = "SINGLEQUOTE"
+    QUOTE = "QUOTE"
+    UNDERSCORE = "UNDERSCORE"
+    DASH = "DASH"
+    PERCENT = "PERCENT"
+    TILDE = "TILDE"
+    PIPE = "PIPE"
+    SEMICOLON = "SEMICOLON"
 
 
 @attr.s
 class FileSplitterJvm(JvmObject):
 
-    name = attr.ib(type=str)
+    splitter = attr.ib(type=FileDataSplitter)
 
     def _create_jvm_instance(self):
-        return self.splitter(self.name) if self.name is not None else None
+        return self.jvm_splitter(self.splitter.value) if self.splitter.value is not None else None
 
     @property
     @require([GeoSparkLib.FileDataSplitter])
-    def splitter(self):
+    def jvm_splitter(self):
         return self.jvm.FileDataSplitter.getFileDataSplitter
