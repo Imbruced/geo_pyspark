b4879081ab4d56d444cb5fc1758d8545b9b528a5
Pawel <pawel93kocinski@gmail.com>
2020-01-05 13:14:24 +0100

    Added _deserialize_geom to abstract parser to create GeoData based on BinaryParser instance.

--- geo_pyspark/utils/abstract_parser.py
+++ geo_pyspark/utils/abstract_parser.py
@@ -8,6 +8,15 @@ from geo_pyspark.utils.binary_parser import BinaryParser, BinaryBuffer
 
 
 @attr.s
+class GeoData:
+    geom = attr.ib(type=BaseGeometry)
+    userData = attr.ib(type=str)
+
+    def getUserData(self):
+        return self.userData
+
+
+@attr.s
 class GeometryParser(ABC):
 
     @property
@@ -33,3 +42,17 @@ class AbstractSpatialRDDParser(ABC):
     @classmethod
     def deserialize(cls, bin_parser: BinaryParser) -> BaseGeometry:
         raise NotImplementedError("Parser has to implement deserialize method")
+
+    @classmethod
+    def _deserialize_geom(cls, bin_parser: BinaryParser) -> GeoData:
+        from geo_pyspark.sql.geometry import GeometryFactory
+
+        user_data_length = bin_parser.read_int()
+        geom = GeometryFactory.geometry_from_bytes(bin_parser)
+        if user_data_length > 0:
+            user_data = bin_parser.read_string(user_data_length)
+            geo_data = GeoData(geom=geom, userData=user_data)
+
+        else:
+            geo_data = GeoData(geom=geom, userData="")
+        return geo_data
