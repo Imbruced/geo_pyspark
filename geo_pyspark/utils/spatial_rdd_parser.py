026e935f2134d864cb93f13431e6b19e95c73c43
Pawel <pawel93kocinski@gmail.com>
2020-01-05 13:17:24 +0100

    Added SpatialPairRDDParserData for parsing RDD[Geometry, Geometry]

--- geo_pyspark/utils/spatial_rdd_parser.py
+++ geo_pyspark/utils/spatial_rdd_parser.py
@@ -1,42 +1,23 @@
 import attr
 from shapely.geometry.base import BaseGeometry
 
-from geo_pyspark.utils.abstract_parser import AbstractSpatialRDDParser
+from geo_pyspark.utils.abstract_parser import AbstractSpatialRDDParser, GeoData
 from geo_pyspark.utils.binary_parser import BinaryParser, BinaryBuffer
 
 
 @attr.s
-class SpatialPairRDDParserNonUserData(AbstractSpatialRDDParser):
-    name = "SpatialRDDParser"
+class SpatialPairRDDParserData(AbstractSpatialRDDParser):
+    name = "SpatialPairRDDParserData"
 
     @classmethod
     def deserialize(cls, bin_parser: BinaryParser):
-        from geo_pyspark.sql.geometry import GeometryFactory
+        left_geom_data = cls._deserialize_geom(bin_parser)
 
-        user_data_length = bin_parser.read_int()
-        left_geom = GeometryFactory.geometry_from_bytes(bin_parser)
-        if user_data_length > 0:
-            user_data = bin_parser.read_string(user_data_length)
-            left_geom_data = GeoData(geom=left_geom, userData=user_data)
+        _ = bin_parser.read_int()
 
-        else:
-            left_geom_data = GeoData(geom=left_geom, userData="")
+        right_geom_data = cls._deserialize_geom(bin_parser)
 
-        geometry_numbers = bin_parser.read_int()
-
-        right_geoms = []
-
-        for right_geometry_number in range(geometry_numbers):
-            user_data_length = bin_parser.read_int()
-            right_geom = GeometryFactory.geometry_from_bytes(bin_parser)
-            if user_data_length > 0:
-                user_data = bin_parser.read_string(user_data_length)
-                right_geom_data = GeoData(geom=right_geom, userData=user_data)
-            else:
-                right_geom_data = GeoData(geom=right_geom, userData="")
-            right_geoms.append(right_geom_data)
-
-        deserialized_data = [left_geom_data, right_geoms] if right_geoms else left_geom_data
+        deserialized_data = [left_geom_data, right_geom_data]
 
         return deserialized_data
 
@@ -46,9 +27,25 @@ class SpatialPairRDDParserNonUserData(AbstractSpatialRDDParser):
 
 
 @attr.s
-class GeoData:
-    geom = attr.ib(type=BaseGeometry)
-    userData = attr.ib(type=str)
+class SpatialRDDParserData(AbstractSpatialRDDParser):
+    name = "SpatialRDDParser"
 
-    def getUserData(self):
-        return self.userData
+    @classmethod
+    def deserialize(cls, bin_parser: BinaryParser):
+        left_geom_data = cls._deserialize_geom(bin_parser)
+
+        geometry_numbers = bin_parser.read_int()
+
+        right_geoms = []
+
+        for right_geometry_number in range(geometry_numbers):
+            right_geom_data = cls._deserialize_geom(bin_parser)
+            right_geoms.append(right_geom_data)
+
+        deserialized_data = [left_geom_data, right_geoms] if right_geoms else left_geom_data
+
+        return deserialized_data
+
+    @classmethod
+    def serialize(cls, obj: BaseGeometry, binary_buffer: BinaryBuffer):
+        raise NotImplementedError("Currently this operation is not supported")
