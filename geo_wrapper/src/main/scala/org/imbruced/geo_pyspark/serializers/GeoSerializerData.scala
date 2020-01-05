cb561041d94887592771ba4173530c87d3e6cdee
Pawel <pawel93kocinski@gmail.com>
2020-01-05 13:18:40 +0100

    Updated serialization to Python for KNN Query.

--- geo_wrapper/src/main/scala/org/imbruced/geo_pyspark/serializers/GeoSerializerData.scala
+++ geo_wrapper/src/main/scala/org/imbruced/geo_pyspark/serializers/GeoSerializerData.scala
@@ -28,6 +28,14 @@ object GeoSerializerData {
 
     ).toJavaRDD()
   }
+  def serializeToPython(geometryList: scala.collection.convert.Wrappers.SeqWrapper[Geometry]): Array[Array[Byte]] = {
+    val sizeBuffer = ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN)
+    sizeBuffer.putInt(0)
+
+    geometryList.toArray.map(
+      geometry => serializeGeomToPython(geometry.asInstanceOf[Geometry]) ++ sizeBuffer.array()
+    )
+  }
 
   def serializeGeomToPython(geom: Geometry): Array[Byte] = {
       val userData = geom.getUserData
@@ -60,7 +68,7 @@ object GeoSerializerData {
       }
     )
   }
-
+  isinstance(types_from_args[0], function_methods[0][0])
   def serializeToPython(spatialRDD: JavaPairRDD[Geometry, Geometry]): JavaRDD[Array[Byte]] = {
     spatialRDD.rdd.map[Array[Byte]](pairRDD =>{
       val leftGeometry = pairRDD._1
