c08016270a8b5e5488b46e4bfb64d39d58f8c607
Pawel <pawel93kocinski@gmail.com>
2020-01-06 11:13:27 +0100

    Changed implementation to allow circle deserialization.

--- geo_pyspark/sql/geometry.py
+++ geo_pyspark/sql/geometry.py
@@ -3,13 +3,11 @@ from typing import List
 import attr
 from shapely.geometry.base import BaseGeometry
 
-from geo_pyspark.sql.enums import GeomEnum
+from geo_pyspark.sql.enums import GeomEnum, ShapeEnum
 from geo_pyspark.sql.exceptions import GeometryUnavailableException
 from geo_pyspark.utils.abstract_parser import GeometryParser
 from geo_pyspark.utils.binary_parser import BinaryParser, BinaryBuffer
-from geo_pyspark.utils.decorators import classproperty
-from geo_pyspark.utils.parsers import MultiPointParser, PolygonParser, PolyLineParser, PointParser, UndefinedParser, \
-    MultiLineStringParser, LineStringParser, MultiPolygonParser
+from geo_pyspark.utils.parsers import CircleParser, PARSERS
 from geo_pyspark.utils.prep import assign_all
 
 
@@ -20,35 +18,27 @@ class GeometryFactory:
     def geometry_from_bytes(cls, bin_parser: BinaryParser) -> BaseGeometry:
         assign_all()
         g_type = bin_parser.read_byte()
-        gm_type = bin_parser.read_byte()
-        if GeomEnum.has_value(gm_type):
-            name = GeomEnum.get_name(gm_type)
-            parser: GeometryParser = cls.parsers[name]
-            geom = parser.deserialize(bin_parser)
-            return geom
-        else:
-            raise GeometryUnavailableException(f"Can not deserialize object")
+        shape_type = ShapeEnum.get_name(g_type)
+
+        if shape_type == ShapeEnum.circle.name:
+            return CircleParser.deserialize(bin_parser)
+
+        elif shape_type == ShapeEnum.shape.name:
+            gm_type = bin_parser.read_byte()
+            if GeomEnum.has_value(gm_type):
+                name = GeomEnum.get_name(gm_type)
+                parser: GeometryParser = PARSERS[name]
+                geom = parser.deserialize(bin_parser)
+                return geom
+            else:
+                raise GeometryUnavailableException(f"Can not deserialize object")
 
     @classmethod
     def to_bytes(cls, geom: BaseGeometry) -> List[int]:
         geom_name = str(geom.__class__.__name__).lower()
 
         try:
-            appr_parser = cls.parsers[geom_name]
+            appr_parser = PARSERS[geom_name]
         except KeyError:
             raise KeyError(f"Parser for geometry {geom_name}")
         return appr_parser.serialize(geom, BinaryBuffer())
-
-    @classproperty
-    def parsers(self):
-        geom_cls = dict(
-            undefined=UndefinedParser,
-            point=PointParser,
-            polyline=PolyLineParser,
-            multilinestring=MultiLineStringParser,
-            linestring=LineStringParser,
-            polygon=PolygonParser,
-            multipoint=MultiPointParser,
-            multipolygon=MultiPolygonParser
-        )
-        return geom_cls
