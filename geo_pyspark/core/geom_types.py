2312d51c5808c913ffe1345a8fb2a8c96147070a
Pawel <pawel93kocinski@gmail.com>
2020-01-06 14:57:51 +0100

    Added JvmCoordinate and JvmPoint to geom_types modules, moved from primitives module.

--- geo_pyspark/core/geom_types.py
+++ geo_pyspark/core/geom_types.py
@@ -1,7 +1,26 @@
 import attr
 
+from geo_pyspark.core.jvm.abstract import JvmObject
 from geo_pyspark.core.utils import require
 from geo_pyspark.register.java_libs import GeoSparkLib
+
+
+@attr.s
+class JvmCoordinate(JvmObject):
+    x = attr.ib(default=0.0)
+    y = attr.ib(default=0.0)
+
+    def _create_jvm_instance(self):
+        return self.jvm.CoordinateFactory.createCoordinates(self.x, self.y)
+
+
+@attr.s
+class JvmPoint(JvmObject):
+    coordinate = attr.ib(type=JvmCoordinate)
+
+    def _create_jvm_instance(self):
+
+        return self.jvm.GeomFactory.createPoint(self.coordinate)
 
 
 @attr.s
@@ -12,7 +31,7 @@ class Envelope:
     maxy = attr.ib()
 
     @require([GeoSparkLib.Envelope])
-    def create_java_object(self, jvm):
+    def create_jvm_instance(self, jvm):
         return jvm.Envelope(
             self.minx, self.maxx, self.miny, self.maxy
         )
@@ -25,5 +44,3 @@ class Envelope:
             miny=java_obj.getMinY(),
             maxy=java_obj.getMaxY(),
         )
-
-Envelope(1, 2, 3, 4)
