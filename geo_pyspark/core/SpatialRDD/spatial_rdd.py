b54edea3722f20dffa6bacb3490aac984a75e2e2
Pawel <pawel93kocinski@gmail.com>
2020-01-11 21:35:28 +0100

    Added JvmPartitioner class.

--- geo_pyspark/core/SpatialRDD/spatial_rdd.py
+++ geo_pyspark/core/SpatialRDD/spatial_rdd.py
@@ -47,28 +47,68 @@ class JvmSpatialRDD:
 
 
 @attr.s
-class JvmPolygonRDD(JvmSpatialRDD):
-    pass
+class JvmPartitioner:
+    jpart = attr.ib()
 
+    def assignPartitionIds(self):
+        raise NotImplementedError("Currently not supported")
 
-@attr.s
-class JvmPointRDD(JvmSpatialRDD):
-    pass
+    def assignPartitionLineage(self):
+        raise NotImplementedError("Currently not supported")
 
+    def dropElements(self):
+        raise NotImplementedError("Currently not supported")
 
-@attr.s
-class JvmLineStringRDD(JvmSpatialRDD):
-    pass
+    def equals(self):
+        raise NotImplementedError("Currently not supported")
 
+    def findZone(self):
+        raise NotImplementedError("Currently not supported")
 
-@attr.s
-class JvmRectangleRDD(JvmSpatialRDD):
-    pass
+    def forceGrowUp(self):
+        raise NotImplementedError("Currently not supported")
 
+    def getAllZones(self):
+        raise NotImplementedError("Currently not supported")
 
-@attr.s
-class JvmCircleRDD(JvmSpatialRDD):
-    pass
+    def getClass(self):
+        raise NotImplementedError("Currently not supported")
+
+    def getElements(self):
+        raise NotImplementedError("Currently not supported")
+
+    def getLeafZones(self):
+        raise NotImplementedError("Currently not supported")
+
+    def getParentZone(self):
+        raise NotImplementedError("Currently not supported")
+
+    def getTotalNumLeafNode(self):
+        raise NotImplementedError("Currently not supported")
+
+    def getZone(self):
+        raise NotImplementedError("Currently not supported")
+
+    def hashCode(self):
+        raise NotImplementedError("Currently not supported")
+
+    def insert(self):
+        raise NotImplementedError("Currently not supported")
+
+    def isLeaf(self):
+        raise NotImplementedError("Currently not supported")
+
+    def notify(self):
+        raise NotImplementedError("Currently not supported")
+
+    def notifyAll(self):
+        raise NotImplementedError("Currently not supported")
+
+    def toString(self):
+        raise NotImplementedError("Currently not supported")
+
+    def wait(self):
+        raise NotImplementedError("Currently not supported")
 
 
 class SpatialRDD:
