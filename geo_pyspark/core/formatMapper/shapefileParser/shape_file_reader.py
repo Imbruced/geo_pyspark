55224ece52839e14a7ff81b8a27c4da0ece556ed
Pawel <pawel93kocinski@gmail.com>
2020-01-19 13:02:29 +0100

    Refactor ShapeFileReader.

--- geo_pyspark/core/formatMapper/shapefileParser/shape_file_reader.py
+++ geo_pyspark/core/formatMapper/shapefileParser/shape_file_reader.py
@@ -2,39 +2,30 @@ import attr
 from pyspark import SparkContext
 
 from geo_pyspark.core.SpatialRDD.spatial_rdd import SpatialRDD
-from geo_pyspark.core.jvm.abstract import JvmObject
+from geo_pyspark.core.formatMapper.geo_reader import GeoDataReader
 from geo_pyspark.core.utils import require
-from geo_pyspark.exceptions import InvalidParametersException
 from geo_pyspark.register.java_libs import GeoSparkLib
+from geo_pyspark.utils.meta import MultipleMeta
 
 
 @attr.s
-class ShapefileReader:
+class ShapefileReader(GeoDataReader, metaclass=MultipleMeta):
 
     @classmethod
     @require([GeoSparkLib.ShapeFileReader])
+    def validate_imports(cls):
+        return True
+
+    @classmethod
     def readToGeometryRDD(cls, sc: SparkContext, inputPath: str) -> SpatialRDD:
-        jvm_shape_file_reader = JvmShapefileReader(jvm=sc._jvm, sparkContext=sc, inputPath=inputPath)
-        return jvm_shape_file_reader.readToGeometryRDD()
+        ShapefileReader.validate_imports()
+        jvm = sc._jvm
+        jsc = sc._jsc
+        srdd = jvm.ShapefileReader.readToGeometryRDD(
+            jsc,
+            inputPath
+        )
+        spatial_rdd = SpatialRDD(sparkContext=sc)
 
-
-@attr.s
-class JvmShapefileReader(JvmObject):
-    sparkContext = attr.ib(default=None, type=SparkContext)
-    inputPath = attr.ib(default=None, type=str)
-
-    def readToGeometryRDD(self) -> SpatialRDD:
-        if self.sparkContext is not None and self.inputPath is not None:
-            jvm = self.sparkContext._jvm
-
-            srdd = jvm.ShapefileReader.readToGeometryRDD(
-                self.sparkContext._jsc,
-                self.inputPath
-            )
-
-            spatial_rdd = SpatialRDD(sparkContext=self.sparkContext)
-            spatial_rdd._srdd = srdd
-            return spatial_rdd
-
-        else:
-            raise InvalidParametersException("Parsed parameters are incorrect")
+        spatial_rdd.set_srdd(srdd)
+        return spatial_rdd
