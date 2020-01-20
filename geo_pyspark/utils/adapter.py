9c7e503cda3439d8c91af30869856fe329b031f2
Pawel <pawel93kocinski@gmail.com>
2020-01-20 21:59:21 +0100

    Fixed issue with toDf method, change to use python rdd.

--- geo_pyspark/utils/adapter.py
+++ geo_pyspark/utils/adapter.py
@@ -4,6 +4,7 @@ from pyspark import RDD
 from pyspark.sql import DataFrame, SparkSession
 
 from geo_pyspark.core.SpatialRDD.spatial_rdd import SpatialRDD, JvmSpatialRDD
+from geo_pyspark.core.enums.spatial import SpatialType
 from geo_pyspark.utils.meta import MultipleMeta
 
 
@@ -19,7 +20,7 @@ class Adapter(metaclass=MultipleMeta):
 
         srdd = jvm.Adapter.toRdd(dataFrame._jdf)
 
-        return srdd.toJavaRDD()
+        return JvmSpatialRDD(srdd.toJavaRDD(), sc, SpatialType.SPATIAL)
 
     @classmethod
     def toSpatialRdd(cls, dataFrame: DataFrame, geometryFieldName: str) -> SpatialRDD:
@@ -57,45 +58,7 @@ class Adapter(metaclass=MultipleMeta):
         return spatial_rdd
 
     @classmethod
-    def toSpatialRdd(cls, dataFrame: DataFrame, geometryColId: int) -> SpatialRDD:
-        """
-
-        :param dataFrame:
-        :param geometryColId:
-        :return:
-        """
-
-        sc = dataFrame._sc
-        jvm = sc._jvm
-
-        srdd = jvm.Adapter.toSpatialRdd(dataFrame._jdf, geometryColId)
-
-        spatial_rdd = SpatialRDD(sc)
-        spatial_rdd.set_srdd(srdd)
-
-        return spatial_rdd
-
-    @classmethod
-    def toSpatialRdd(cls, dataFrame: DataFrame, geometryColId: int, fieldNames: List[str]) -> SpatialRDD:
-        """
-
-        :param dataFrame:
-        :param geometryColId:
-        :param fieldNames:
-        :return:
-        """
-        sc = dataFrame._sc
-        jvm = sc._jvm
-
-        srdd = jvm.Adapter.toSpatialRdd(dataFrame._jdf, geometryColId, fieldNames)
-
-        spatial_rdd = SpatialRDD(sc)
-        spatial_rdd.set_srdd(srdd)
-
-        return spatial_rdd
-
-    @classmethod
-    def toSpatialRdd(cls, dataFrame: DataFrame, geometryFieldName: str, fieldNames: List[str]) -> SpatialRDD:
+    def toSpatialRdd(cls, dataFrame: DataFrame, fieldNames: List[str]) -> SpatialRDD:
         """
 
         :param dataFrame:
@@ -106,7 +69,7 @@ class Adapter(metaclass=MultipleMeta):
         sc = dataFrame._sc
         jvm = sc._jvm
 
-        srdd = jvm.Adapter.toSpatialRdd(dataFrame._jdf, geometryFieldName, fieldNames)
+        srdd = jvm.AdapterWrapper.toSpatialRdd(dataFrame._jdf, fieldNames)
 
         spatial_rdd = SpatialRDD(sc)
         spatial_rdd.set_srdd(srdd)
@@ -125,7 +88,7 @@ class Adapter(metaclass=MultipleMeta):
         sc = spatialRDD._sc
         jvm = sc._jvm
 
-        jdf = jvm.Adapter.toDf(spatialRDD._srdd, fieldNames, sparkSession._jsparkSession)
+        jdf = jvm.AdapterWrapper.toDf(spatialRDD._srdd, fieldNames, sparkSession._jsparkSession)
 
         df = DataFrame(jdf, sparkSession._wrapped)
 
@@ -173,21 +136,10 @@ class Adapter(metaclass=MultipleMeta):
         :return:
         """
 
-        sc = spatialPairRDD.ctx
-        jvm = sc._jvm
-
-        left_field_names_jvm = jvm.java.util.ArrayList()
-        right_field_names_jvm = jvm.java.util.ArrayList()
-
-        for left_element in leftFieldnames:
-            left_field_names_jvm.add(left_element)
-
-        for right_element in rightFieldNames:
-            right_field_names_jvm.add(right_element)
-        jdf = jvm.Adapter.toDf(
-            spatialPairRDD._jrdd, left_field_names_jvm.toList, right_field_names_jvm.toList, sparkSession._jsparkSession
-        )
-
-        df = DataFrame(jdf, sparkSession._wrapped)
-
-        return df
+        df = Adapter.toDf(spatialPairRDD, sparkSession)
+        columns_length = df.columns.__len__()
+        combined_columns = ["geom_1", *leftFieldnames, "geom_2", *rightFieldNames]
+        if columns_length == combined_columns.__len__():
+            return df.toDF(*combined_columns)
+        else:
+            raise TypeError("Column length does not match")
