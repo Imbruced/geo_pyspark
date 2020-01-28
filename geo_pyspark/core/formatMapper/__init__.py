f60c048e6ed6aa4c71b1ff5af8cfc34d5027c837
Pawel <pawel93kocinski@gmail.com>
2020-01-28 21:31:38 +0100

    Added WkbReader to init.

--- geo_pyspark/core/formatMapper/__init__.py
+++ geo_pyspark/core/formatMapper/__init__.py
@@ -1,4 +1,5 @@
 from .geo_json_reader import GeoJsonReader
 from .wkt_reader import WktReader
+from .wkb_reader import WkbReader
 
-__all__ = ["GeoJsonReader", 'WktReader']
+__all__ = ["GeoJsonReader", 'WktReader', 'WkbReader']
